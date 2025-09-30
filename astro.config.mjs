df5bafec5542c5707de86cfe64d5b83d8ef22531
semajssor <141041402+semajssor@users.noreply.github.com>
2025-09-30 15:59:11 +0200

    Add GitHub Actions for Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://semajssor.github.io/adsgestion/",
+  base: "/adsgestion/",
   integrations: [react()]
 });
