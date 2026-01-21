07139300c27d47ebcd31b2015c1145e6e5f9afd1
semajssor <141041402+semajssor@users.noreply.github.com>
2026-01-21 22:06:56 +0100

    changed the booking redirect and URL

---
+++ src/constants.jsx
@@ -0,0 +1,15 @@
+export const SITE_METADATA = {
+   title: "ADS Gestion Financière",
+   url: "https://adsgestion.ca",
+};
+
+export const CONTACT_INFO = {
+   email: "info@adsgestion.ca", 
+   phone: "(514) 824-8131",
+   bookingUrl: "/rendez-vous",
+};
+
+export const SOCIAL_LINKS = {
+   //linkedin: "",
+   // facebook: "...",
+};
