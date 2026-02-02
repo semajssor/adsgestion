e3569ad54e651f6c58fcb5a7e5b12abbd0746120
semajssor <141041402+semajssor@users.noreply.github.com>
2026-02-03 00:08:03 +0100

    added sitemap and robots and collect-de-donnees page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-	integrations: [react()],
+	site: 'https://adsgestion.ca',
+	integrations: [react(), sitemap()],
 });
