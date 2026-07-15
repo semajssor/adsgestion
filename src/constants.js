1a261ac146418deae38b9f7434b5f2c913b022c5
semajssor <141041402+semajssor@users.noreply.github.com>
2026-07-15 16:11:24 +0200

    updated info + politique de confidentialité with conseils fortune

--- src/constants.js
+++ src/constants.js
@@ -1,12 +1,12 @@
 export const SITE_METADATA = {
-   title: "ADS Gestion Financière",
+   title: "Audrey Samson - Conseils fortune",
    url: "https://adsgestion.ca",
 };
 
 export const CONTACT_INFO = {
    email: "adminsitration@adsgestion.ca", 
    phone: "(514) 824-8131",
-   bookingUrl: "/rendez-vous",
+   //bookingUrl: "/rendez-vous",
 };
 
 export const SOCIAL_LINKS = {
